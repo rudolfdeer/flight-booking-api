7144b2ae87a1eebc3cf1480d7928c38208abb757
rudolfdeer <tomalichko@gmail.com>
2022-02-22 16:41:09 +0300

    feat: add flight requests

--- flight-booking-api/lib/modules/flight/flight.controller.ts
+++ flight-booking-api/lib/modules/flight/flight.controller.ts
@@ -1,9 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Flight } from './flight.entity';
+import { FlightsService } from './flight.service';
 
 @Controller('flights')
 export class FlightsController {
+  constructor(private readonly flightsService: FlightsService) {}
+
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   findAll() {
-    //
+    return this.flightsService.findAll();
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/:id')
+  getFlight(@Param('id') id: string) {
+    return this.flightsService.findById(id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  create(@Body() body: Flight, @Request() req) {
+    return this.flightsService.create(body);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put('/:id')
+  update(@Body() body: Flight, @Param('id') id: string) {
+    return this.flightsService.update(body, id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('/:id')
+  delete(@Param('id') id: string) {
+    return this.flightsService.delete(id);
   }
 }
--- flight-booking-api/lib/modules/flight/flight.service.ts
+++ flight-booking-api/lib/modules/flight/flight.service.ts
@@ -1,13 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { Flight } from './flight.entity';
-
-
 
 @Injectable()
 export class FlightsService {
-  private readonly flights: Flight[] = [];
+  constructor(
+    @InjectRepository(Flight)
+    private flightsRepository: Repository<Flight>,
+  ) {}
 
-  findAll() {
-    //
+  async findAll() {
+    const flights = await this.flightsRepository.find();
+
+    return flights;
+  }
+
+  async findById(id: string) {
+    const flight = await this.flightsRepository.findOne(id);
+
+    return flight;
+  }
+
+  async create(body: Flight) {
+    const newFlight = { ...body };
+
+    const flight = await this.flightsRepository.save(newFlight);
+
+    return flight;
+  }
+
+  async update(body: Flight, id: string) {
+    await this.flightsRepository.update(id, body);
+
+    return this.findById(id);
+  }
+
+  async delete(id: string) {
+    await this.flightsRepository.delete(id);
+
+    return 'deleted';
   }
 }
