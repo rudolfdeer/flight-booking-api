a7187443b76ffe13d3e06a06fc2cdd24d2685d24
rudolfdeer <tomalichko@gmail.com>
2022-02-22 15:44:52 +0300

    feat: add tickets requests

--- flight-booking-api/lib/modules/ticket/ticket.controller.ts
+++ flight-booking-api/lib/modules/ticket/ticket.controller.ts
@@ -1,9 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { TicketsService } from './ticket.service';
 
 @Controller('tickets')
 export class TicketsController {
-  @Get()
-  findAll() {
-    //
+  constructor(private readonly ticketsService: TicketsService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/booked')
+  async getBookedTickets(@Request() req) {
+    const userId = req.user.id;
+    return this.ticketsService.getBookedTickets(userId);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/booked/:id')
+  async getTicket(@Param('id') id: string) {
+    return this.ticketsService.findById(id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('/booked/:id')
+  async bookTicket(@Request() req, @Param('id') id: string) {
+    const userId = req.user.id;
+    return this.ticketsService.bookTicket(id, userId);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('/booked/:id')
+  async deleteBookedTicket(@Request() req, @Param('id') id: string) {
+    const userId = req.user.id;
+    return this.ticketsService.deleteBookedTicket(id, userId);
   }
 }
