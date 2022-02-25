e786e9001d6dea70f2537726b1946564c2ddaf54
rudolfdeer <tomalichko@gmail.com>
2022-02-25 13:00:39 +0300

    feat: add config module

--- flight-booking-api/lib/app.module.ts
+++ flight-booking-api/lib/app.module.ts
@@ -3,13 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FlightModule } from './modules/flight/flight.module';
 import { ProfileModule } from './modules/profile/profile.module';
 import { TicketModule } from './modules/ticket/ticket.module';
-import "reflect-metadata";
+import 'reflect-metadata';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       autoLoadEntities: true,
     }),
+    ConfigModule.forRoot(),
     ProfileModule,
     FlightModule,
     TicketModule,
